9a2e65e84b299b097a32b70dbacdce48ff04d71e
ildi-ildi <ildikoszabo@yahoo.com>
2017-04-13 14:35:25 +0100

    add method

--- Site/src/Site.java
+++ Site/src/Site.java
@@ -1,5 +1,7 @@
 public class Site {
     
+    public class PageNotFoundException extends Exception{}
+    
     private class PageNode{
         private String name;
         private PageNode up;
@@ -15,4 +17,42 @@ public class Site {
         this.home.name = homePage;
         this.current = this.home;
     }
+    
+    public String toString() {
+        String pageDetails = new String();
+        pageDetails += this.home.name + "\n";
+        PageNode pageAcross = this.home.down;
+        if (pageAcross == null) {
+            pageDetails += "  has no page\n";
+        } else {
+            while (pageAcross != null) {
+                pageDetails += "  " + pageAcross.name + "\n";
+                pageAcross = pageAcross.across;
+            }
+        }
+        return pageDetails;
+    }
+    
+    public void addSite(String name) throws PageNotFoundException{
+        PageNode newNode = new PageNode();
+        newNode.name = name;
+        if (this.home.down == null) {
+            this.home.down = newNode;
+        } else {
+            this.addSite(newNode,this.home.down);
+        }
+    }
+    
+   private void addSite(PageNode newNode,PageNode current) throws PageNotFoundException{
+       
+        if (newNode.name.compareTo(current.name) == 0){
+            throw new PageNotFoundException();
+        }
+            if (current.across == null) {
+                current.across = newNode;
+            } else {
+                this.addSite(newNode,current.across);
+            }      
+        }
+    
 }
--- Site/src/SiteTest.java
+++ Site/src/SiteTest.java
@@ -7,9 +7,26 @@ public class SiteTest {
          
          Integer option;
         do {
+            System.out.println("0: quit");
+            System.out.println("1: add page");
+            System.out.println("2: display pages");
+            option = Input.getInteger("input option: ");
             
-            
-            
+            switch (option) {
+                case 1:
+                    name = Input.getString("input page's name: ");
+                    try{
+                    site.addSite(name);
+                    }catch(Site.PageNotFoundException e){
+                        System.out.println("insert invalid - value not unique");
+                    }
+                    break;
+                case 2:
+                    System.out.println(site);
+                    break;
+                case 0:
+                    break;
+            }
         }while (option != 0);
      }
 }
