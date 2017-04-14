92380febbf9efd1a05e21abfb7e7f1bf96973a9b
ildi-ildi <ildikoszabo@yahoo.com>
2017-04-14 23:02:26 +0100

    init files fix

---
+++ Site/src/Site.java
@@ -0,0 +1,165 @@
+
+public class Site {
+
+    public class PageNotFoundException extends Exception {
+    }
+
+    public class NameNotFoundException extends Exception {
+    }
+
+    public class NameNotUniqueException extends Exception {
+    }
+    
+    public class TopPageException extends Exception {
+    }
+    
+    private class PageNode {
+
+        private String name;
+        private PageNode up;
+        private PageNode across;
+        private PageNode down;
+    }
+
+    private PageNode home;
+    private PageNode current; 
+    private PageNode currentPage;
+
+    public Site(String homePage) {
+        this.home = new PageNode();
+        this.home.name = homePage;
+        this.current = this.home;
+        this.currentPage = this.home;
+    }
+
+    public String toString() {
+        String pageDetails = new String();
+        pageDetails += this.home.name + "\n";
+        PageNode pageAcross = this.home.down;
+        if (pageAcross == null) {
+            pageDetails += "  has no links\n";
+        } else {
+            while (pageAcross != null) {
+                pageDetails += "  " + pageAcross.name + "\n";
+                pageAcross = pageAcross.across;
+            }
+        }
+        return pageDetails;
+    }
+
+    public void addPage(String name) throws NameNotUniqueException {
+
+        if (isUnique(name) == false) {
+            throw new NameNotUniqueException();
+        }
+
+        PageNode newNode = new PageNode();
+        newNode.name = name;
+        if (this.currentPage.down == null) { //mda...
+            this.currentPage.down = newNode;
+            newNode.up = this.currentPage;
+        } else {
+            this.addPage(newNode, this.currentPage.down);
+        }
+    }
+
+    private void addPage(PageNode newNode, PageNode current) {
+
+        if (current.across != null) {
+            current = current.across;
+            this.addPage(newNode, current);
+        } else {
+            current.across = newNode;
+            newNode.up = this.currentPage;
+        }
+    }
+
+    public Boolean isUnique(String pageName) {
+        return this.isUnique(pageName, this.home);
+    }
+
+    private Boolean isUnique(String pageName, PageNode next) {
+
+        Boolean found = true;
+
+        if (found == true) {
+            if (pageName.compareTo(next.name) == 0) {
+                found = false; // found something identical 
+            } else {
+                if (next.down != null) {
+                    found = this.isUnique(pageName, next.down);
+                } else if (next.across != null) {
+                    found = this.isUnique(pageName, next.across);
+                }
+            }
+        }
+        return found;
+    }
+
+    public String getCurrent() {
+        String currentDetails = new String();
+        currentDetails += this.currentPage.name + "\n";
+        PageNode pageAcross = this.currentPage.down;
+        if (pageAcross == null) {
+            currentDetails += "  has no link\n";
+        } else {
+            while (pageAcross != null) {
+                currentDetails += "  " + pageAcross.name + "\n";
+                pageAcross = pageAcross.across;
+            }
+        }
+
+        return currentDetails;
+    }
+
+    public Boolean checkIfHasCurrentDown() throws PageNotFoundException {
+        Boolean searching;
+        if (this.currentPage.down == null) {
+            throw new PageNotFoundException();
+        } else {
+            searching = true; //found
+        }
+        return searching;
+    }
+
+    public void moveDown() {
+        this.currentPage = this.currentPage.down;
+    }
+
+    public String findSite(String siteName) throws NameNotFoundException {
+        return this.findSite(siteName, this.currentPage);
+    }
+
+    private String findSite(String siteName, PageNode currentPage) throws NameNotFoundException {
+
+        String found = " ";
+        if (currentPage != null) {
+
+            if (siteName.compareTo(currentPage.name) == 0) {
+                this.currentPage = currentPage;
+                found = currentPage.name;
+            } else {
+                found = this.findSite(siteName, currentPage.across);
+            }
+
+        } else {
+            try {
+                moveUp();
+                throw new NameNotFoundException();
+            } catch (TopPageException e) {
+                System.out.println("page up invalid - home page ");
+            }
+        }
+
+        return found;
+    }
+
+    public void moveUp() throws TopPageException {
+        if(this.currentPage == this.currentPage.up){
+            throw new TopPageException();
+        }else{
+            this.currentPage = this.currentPage.up;
+        }
+    }
+
+}
---
+++ Site/src/SiteTest.java
@@ -0,0 +1,60 @@
+ 
+public class SiteTest {
+    
+     public static void main(String[] args){
+         String name = Input.getString("input home page's name: ");
+         Site site = new Site(name);
+         
+         Integer option;
+        do {
+            System.out.println("0: quit");
+            System.out.println("1: add page");
+            System.out.println("2: move down");
+            System.out.println("3: move up");
+            System.out.println("4: display current page content");
+            System.out.println("5: display site map");
+            option = Input.getInteger("input option: ");
+            
+            switch (option) {
+                case 1:
+                    name = Input.getString("input page's name: ");
+                    try{
+                    site.addPage(name);
+                    }catch(Site.NameNotUniqueException e){
+                        System.out.println("insert invalid - value not unique");
+                    }
+                    break;
+                case 2:
+                    System.out.println(site.getCurrent());
+                    try{
+                    if(site.checkIfHasCurrentDown() == true){
+                        String namePage = Input.getString("page name: ");
+                        site.moveDown();
+                        site.findSite(namePage);
+                    }
+                    }catch(Site.PageNotFoundException e){
+                        System.out.println("page down invalid - current page has no links");
+                    }catch(Site.NameNotFoundException e){
+                        System.out.println("page down invalid - no match found for page ");
+                    }
+                    
+                    break;
+                case 3:
+                    try {
+                        site.moveUp();
+                    } catch (Site.TopPageException e) {
+                        System.out.println("page up invalid - home page ");
+                    }
+                    break;
+                case 4:
+                    System.out.println(site.getCurrent());
+                    break;
+                case 5:
+                    System.out.println(site);
+                    break;
+                case 0:
+                    break;
+            }
+        }while (option != 0);
+     }
+}
